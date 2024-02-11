39f938651850b293f7798f59baf2774988ae3fbb
spradhan7656 <spradhan7656@gmail.com>
2024-02-11 21:36:55 +0530

    complete weather app

---
+++ src/main/java/myPackage/Myservlet.java
@@ -0,0 +1,113 @@
+package myPackage;
+
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+/**
+ * Servlet implementation class Myservlet
+ */
+public class Myservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Myservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+		//Api Setup
+		String apiKey="f8cd248d27b78c0133eb0962dc4bffd6";
+		//Get the city form the form input
+		String city=request.getParameter("city");
+		//Create the URL for the Open Weather api request
+		String apiURL="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey;
+		//API integration
+		URL url=new URL(apiURL); 
+		HttpURLConnection connection=(HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+		//Raading the data from network
+		InputStream inputStream=connection.getInputStream();
+		InputStreamReader reader=new InputStreamReader(inputStream);
+		
+		//want to store in string
+		StringBuilder responseContent = new StringBuilder();
+		
+		//input form the reader
+		Scanner scanner =new Scanner(reader);
+		
+		while(scanner.hasNext())
+		{
+			responseContent.append(scanner.nextLine());
+		}
+		
+		scanner.close();
+		//TypeCasting = parasing the data into JSON
+		Gson gson=new Gson();
+		JsonObject jsonObject=gson.fromJson(responseContent.toString(),JsonObject.class);
+		
+		//date & time
+		  long dateTimestamp = jsonObject.get("dt").getAsLong() * 1000;
+          String date = new Date(dateTimestamp).toString();
+         
+		//Temperature
+		double temperatureKelvin = jsonObject.getAsJsonObject("main").get("temp").getAsDouble();
+        int temperatureCelsius = (int) (temperatureKelvin - 273.15);
+		//Humidity
+        int humidity=jsonObject.getAsJsonObject("main").get("humidity").getAsInt();
+        //wind Speed
+        double windSpeed=jsonObject.getAsJsonObject("wind").get("speed").getAsDouble();
+        //weather condition
+        String weatherCondition =jsonObject.getAsJsonArray("weather").get(0).getAsJsonObject().get("main").getAsString();
+        
+       
+       
+        
+
+        // Set the data as request attributes (for sending to the jsp page)
+        request.setAttribute("date", date);
+        request.setAttribute("city", city);
+        request.setAttribute("temperature", temperatureCelsius);
+        request.setAttribute("weatherCondition", weatherCondition); 
+        request.setAttribute("humidity", humidity);    
+        request.setAttribute("windSpeed", windSpeed);
+        request.setAttribute("weatherData", responseContent.toString());
+	
+        connection.disconnect();
+        // Forward the request to the weather.jsp page for rendering
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+        
+	}
+	
+
+}
